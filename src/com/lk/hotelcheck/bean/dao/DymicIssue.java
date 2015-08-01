85ee3eb5cc61cb2595bef243848bfa4bb7dc730b
snake326 <674993264@qq.com>
2015-08-01 23:11:48 +0800

    add dymic issue

---
+++ src/com/lk/hotelcheck/bean/dao/DymicIssue.java
@@ -0,0 +1,95 @@
+package com.lk.hotelcheck.bean.dao;
+
+import com.lk.hotelcheck.bean.IssueItem;
+import com.orm.SugarRecord;
+
+public class DymicIssue extends SugarRecord<DymicIssue>{
+	private long checkId;
+	private long areaId;
+	private String name;
+	private int dimOneId;
+	private String dimOneName;
+	
+	public DymicIssue() {
+		super();
+	}
+	
+	
+	
+	
+	public DymicIssue(long checkId, long areaId, String name, int dimOneId,
+			String dimOneName) {
+		super();
+		this.checkId = checkId;
+		this.areaId = areaId;
+		this.name = name;
+		this.dimOneId = dimOneId;
+		this.dimOneName = dimOneName;
+	}
+
+
+
+
+	public DymicIssue(long checkId,long areaId, IssueItem issueItem) {
+		this.checkId = checkId;
+		this.areaId = areaId;
+		this.id = (long) issueItem.getId();
+		this.name = issueItem.getName();
+		this.dimOneId = issueItem.getDimOneId();
+		this.dimOneName = issueItem.getDimOneName();
+	}
+
+
+
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getDimOneId() {
+		return dimOneId;
+	}
+	public void setDimOneId(int dimOneId) {
+		this.dimOneId = dimOneId;
+	}
+	public String getDimOneName() {
+		return dimOneName;
+	}
+	public void setDimOneName(String dimOneName) {
+		this.dimOneName = dimOneName;
+	}
+
+
+
+
+	public long getCheckId() {
+		return checkId;
+	}
+
+
+
+
+	public void setCheckId(long checkId) {
+		this.checkId = checkId;
+	}
+
+
+
+
+	public long getAreaId() {
+		return areaId;
+	}
+
+
+
+
+	public void setAreaId(long areaId) {
+		this.areaId = areaId;
+	}
+	
+	
+	
+	
+}
